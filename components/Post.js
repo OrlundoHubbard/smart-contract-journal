d29aeb426b7abe57b8205dd9246063f9562d7707
Orlundo Hubbard <orlundo@wursta.com>
2022-07-22 18:37:11 -0500

    template commit

---
+++ components/Post.js
@@ -0,0 +1,17 @@
+/* eslint-disable jsx-a11y/alt-text */
+import Link from "next/link";
+
+export default function Post({ post }) {
+  return (
+    <div className="card">
+      <img src={post.frontmatter.cover_image} />
+      <div className="post-date">Posted on {post.frontmatter.date}</div>
+      <h3>{post.frontmatter.title}</h3>
+      <p>{post.frontmatter.excerpt}</p>
+
+      <Link href={`/blog/${post.slug}`}>
+        <a className="btn">Read More</a>
+      </Link>
+    </div>
+  );
+}
